240fb87cd2d42ab5bd7f8d478504ba36d0c499ed
Anders Langhoff <Andylanghoff@gmail.com>
2024-11-22 15:46:45 +0000

    test commit

--- restaurant_booking/restaurant_booking/urls.py
+++ restaurant_booking/restaurant_booking/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('reservations.urls')),  # Correctly include the reservations URLs
+    path('', include('reservations.urls')),  # Include URLs from the reservations app
 ]
